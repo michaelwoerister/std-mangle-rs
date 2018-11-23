f9d55d2ede4e96dc903f8ff5419b4ad330bb39fb
Michael Woerister <michaelwoerister@posteo>
2018-11-23 17:42:52 +0100

    Rename the compress module to compress_fast.

--- src/quickcheck_testing/mod.rs
+++ src/quickcheck_testing/mod.rs
@@ -62,7 +62,7 @@ quickcheck! {
     fn demangle_direct_verbose(symbol: Symbol) -> bool {
         let expected = ::ast_to_demangled_symbol(&symbol, true);
         let uncompressed_mangled = ::ast_to_mangled_symbol(&symbol);
-        let (compressed_symbol, compression_state) = ::compress::compress_ext(&symbol);
+        let (compressed_symbol, compression_state) = ::compress_fast::compress_ext(&symbol);
         let compressed_mangled =::ast_to_mangled_symbol(&compressed_symbol);
         let (actual, demangling_dict) =  ::direct_demangle::Demangler::demangle_debug(compressed_mangled.as_bytes(), true);
 
@@ -89,7 +89,7 @@ quickcheck! {
     fn demangle_direct(symbol: Symbol) -> bool {
         let expected = ::ast_to_demangled_symbol(&symbol, false);
         let uncompressed_mangled = ::ast_to_mangled_symbol(&symbol);
-        let (compressed_symbol, compression_state) = ::compress::compress_ext(&symbol);
+        let (compressed_symbol, compression_state) = ::compress_fast::compress_ext(&symbol);
         let compressed_mangled =::ast_to_mangled_symbol(&compressed_symbol);
         let (actual, demangling_dict) =  ::direct_demangle::Demangler::demangle_debug(compressed_mangled.as_bytes(), false);
 
@@ -114,7 +114,7 @@ quickcheck! {
 
 quickcheck! {
     fn compress_decompress_leaves_ast_unchanged(symbol: Symbol) -> bool {
-        let (compressed, compression_state) = ::compress::compress_ext(&symbol);
+        let (compressed, compression_state) = ::compress_fast::compress_ext(&symbol);
         let (decompressed, decompression_state) = ::decompress::decompress_ext(&compressed);
 
         if symbol != decompressed {
@@ -152,6 +152,29 @@ quickcheck! {
             symbol,
             decompressed,
             compressed)
+        }
+
+        true
+    }
+}
+
+quickcheck! {
+    fn compress_ref_and_compress_fast_have_same_result(symbol: Symbol) -> bool {
+        use ast_demangle::AstDemangle;
+
+        let (compressed_ref, compression_state_ref) = ::compress_ref::compress_ext(&symbol);
+        let (compressed_fast, compression_state_fast) = ::compress_fast::compress_ext(&symbol);
+
+        if compressed_ref != compressed_fast {
+            let compression_dict_ref = compression_state_ref.to_debug_dictionary();
+            let compression_dict_fast = compression_state_fast.to_debug_dictionary();
+
+            compression_dict_fast.print_comparison(&compression_dict_ref);
+
+            panic!("compressed(ref):  {:?}\n\
+                    compressed(fast): {:?}\n",
+            compressed_ref.demangle(true),
+            compressed_fast.demangle(true))
         }
 
         true
